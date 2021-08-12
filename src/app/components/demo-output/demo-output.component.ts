2cd99b0d141557dac186fc014b51c4675b35b0ca
elwood-berry <eberry@anterasoftware.com>
2021-08-12 14:10:28 -0500

    4 files added

---
+++ src/app/components/demo-output/demo-output.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-output',
+  templateUrl: './demo-output.component.html',
+  styleUrls: ['./demo-output.component.css']
+})
+export class DemoOutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
